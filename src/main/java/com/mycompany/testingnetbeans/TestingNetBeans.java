c0bd86a213ad6c6841182086867c1de0411ea2ce
Char <paparis060@gmail.com>
2023-02-27 12:37:09 -0300

    git repository created

---
+++ src/main/java/com/mycompany/testingnetbeans/TestingNetBeans.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testingnetbeans;
+
+/**
+ *
+ * @author papar
+ */
+public class TestingNetBeans {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
